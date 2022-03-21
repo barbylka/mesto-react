a619674e854fd3c778997edec9932e0a757f0598
Irina Stafeeva <irina.staffeewa@gmail.com>
2022-03-21 15:27:55 +0100

    refactor: prettier code style+clean and format jsx

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React from "react";
 
-function PopupWithForm(props) {
-  const className = `popup popup_type_${props.name} ${props.isOpen && 'popup_opened'}`
-  const formName = `popup__container popup__container_type_${props.name}`
+function PopupWithForm({ name, isOpen, onClose, title, children }) {
+  const popupClassName = `popup popup_type_${name} ${isOpen && "popup_opened"}`;
+  const formClassName = `popup__container popup__container_type_${name}`;
 
   return (
-    <section className={className}>
+    <section className={popupClassName}>
       <div className="popup__overlay"></div>
-      <form className={formName} name={`${props.name}`} noValidate>
-        <button className="popup__exit-button" type="button" onClick={props.onClose}></button>
-        <h2 className="popup__title">{`${props.title}`}</h2>
-        {props.children}
-        <button className="popup__save-button" type="submit">Сохранить</button>
+      <form className={formClassName} name={name} noValidate>
+        <button
+          className="popup__exit-button"
+          type="button"
+          onClick={onClose}
+        ></button>
+        <h2 className="popup__title">{title}</h2>
+        {children}
+        <button className="popup__save-button" type="submit">
+          Сохранить
+        </button>
       </form>
     </section>
-  )
+  );
 }
 
 export default PopupWithForm;
